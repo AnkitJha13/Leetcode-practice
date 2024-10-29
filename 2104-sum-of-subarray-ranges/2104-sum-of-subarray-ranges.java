31f2f60ca19dfcdd721b2aaa5537d0ff70809899
ANKIT KUMAR JHA <116744896+AnkitJha13@users.noreply.github.com>
2024-10-29 18:04:30 +0530

    Time: 18 ms (60.23%), Space: 44 MB (79.32%) - LeetHub

---
+++ 2104-sum-of-subarray-ranges/2104-sum-of-subarray-ranges.java
@@ -0,0 +1,41 @@
+class Solution {
+    public long subArrayRanges(int[] nums) {
+        Stack<Integer> st = new Stack<>();
+        long sumOfMinimums = 0;
+        long sumOfMaximums = 0;
+
+        // sum of minimums for all subarrays
+        for (int i = 0; i <= nums.length; i++) {
+            while (!st.isEmpty() && (i == nums.length || nums[st.peek()] >= nums[i])) {
+                int mid = st.pop();
+                int leftBoundary = st.isEmpty() ? -1 : st.peek();
+                int rightBoundary = i;
+                long subarraysLeft = mid - leftBoundary;
+                long subarraysRight = rightBoundary - mid;
+                long count = subarraysLeft * subarraysRight;
+                sumOfMinimums += count * nums[mid];
+            }
+            st.push(i);
+        }
+
+        // Reset the stack
+        st.clear();
+
+        // sum of maximums for all subarrays
+        for (int i = 0; i <= nums.length; i++) {
+            while (!st.isEmpty() && (i == nums.length || nums[st.peek()] <= nums[i])) {
+                int mid = st.pop();
+                int leftBoundary = st.isEmpty() ? -1 : st.peek();
+                int rightBoundary = i;
+                long subarraysLeft = mid - leftBoundary;
+                long subarraysRight = rightBoundary - mid;
+                long count = subarraysLeft * subarraysRight;
+                sumOfMaximums += count * nums[mid];
+            }
+            st.push(i);
+        }
+
+        //  difference 
+        return sumOfMaximums - sumOfMinimums;
+    }
+}
