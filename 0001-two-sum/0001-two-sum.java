b6fa1e75bd214e3de74a334c7f3125583e1114cb
ANKIT KUMAR JHA <116744896+AnkitJha13@users.noreply.github.com>
2025-01-02 20:05:28 +0530

    Update and rename 0001-two-sum.cpp to 0001-two-sum.java

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -1,17 +1,16 @@
 class Solution {
-public:
-    vector<int> twoSum(vector<int>& nums, int target) {
-         int n = nums.size();
+    public int[] twoSum(int[] nums, int target) {
+        int n = nums.length;
          for(int i=0;i<n-1;i++)
          {
              for(int j=i+1;j<n;j++)
              {
                  if(nums[i]+nums[j]==target)
                  {
-                     return {i,j};
+                     return new int[]{i,j};
                  }
              }
          }
-         return {};
+         return new int[]{};
     }
-};
+}
