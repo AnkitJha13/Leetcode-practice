bb7f91f400162316211b82452ec57b18821b4a72
ANKIT KUMAR JHA <116744896+AnkitJha13@users.noreply.github.com>
2025-03-16 22:56:21 +0530

    Time: 0 ms (100%), Space: 44.8 MB (82.6%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.java
+++ 0733-flood-fill/0733-flood-fill.java
@@ -1,28 +1,25 @@
 class Solution {
-    private void dfs(int[][] image, int r, int c, int originalColor, int newColor) {
-        // Boundary checks and color check
-        if (r < 0 || r >= image.length || c < 0 || 
-              c >= image[0].length || image[r][c] != originalColor) {
+    private void dfs(int[][] image, int r, int c, int originalColor, int newColor){
+
+        if(r<0 || r>=image.length || c<0 || c>=image[0].length || image[r][c] != originalColor){
             return;
         }
 
-        
         image[r][c] = newColor;
 
-        // Move to adjacent pixels (up, down, left, right)
-        
-        dfs(image, r, c - 1, originalColor, newColor); // Left
-        dfs(image, r, c + 1, originalColor, newColor); // Right
-        dfs(image, r - 1, c, originalColor, newColor); // Up
-        dfs(image, r + 1, c, originalColor, newColor); // Down
-        
+        dfs(image, r, c-1, originalColor, newColor);
+        dfs(image, r, c+1, originalColor, newColor);
+        dfs(image, r-1, c, originalColor, newColor);
+        dfs(image, r+1, c, originalColor, newColor);
     }
 
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         int originalColor = image[sr][sc];
-        if (originalColor != color) {
+
+        if(originalColor != color){
             dfs(image, sr, sc, originalColor, color);
         }
+
         return image;
     }
 }
