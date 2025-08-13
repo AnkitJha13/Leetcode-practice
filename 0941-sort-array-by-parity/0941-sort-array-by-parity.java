ad9acfdf8a39f3e74cc6d467af8a9614977ad589
ANKIT KUMAR JHA <116744896+AnkitJha13@users.noreply.github.com>
2025-08-13 22:53:55 +0530

    Time: 0 ms (100%), Space: 45.1 MB (41.71%) - LeetHub

--- 0941-sort-array-by-parity/0941-sort-array-by-parity.java
+++ 0941-sort-array-by-parity/0941-sort-array-by-parity.java
@@ -1,15 +1,16 @@
 class Solution {
     public int[] sortArrayByParity(int[] nums) {
-        int j = 0; // to track even numbers
+        int j = 0 ;
 
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] % 2 == 0) { 
+        for(int i=0; i<nums.length; i++){
+            if(nums[i] % 2 == 0){
                 int temp = nums[i];
                 nums[i] = nums[j];
                 nums[j] = temp;
-                j++; // Move the pointer forward for next even number
+                j++;
             }
         }
-        return nums; // Return sorted array
+
+        return nums;
     }
 }
